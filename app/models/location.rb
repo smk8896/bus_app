f1afb0817596276dc311f678da5227f2abb4dff1
Shea Matthew Kennisher <deidrelarroque@Deidres-MacBook-Air.local>
2017-06-29 08:47:51 -0500

    Add cities to dropdown form

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,32 @@ class Location < ApplicationRecord
   geocoded_by :full_street_address
   after_validation :geocode
 
+  MARTA_CITIES = ['Atlanta',
+                  'Alpharetta',
+                  'Avondale Estates',
+                  'Chamblee',
+                  'Clarkston',
+                  'College Park',
+                  'Decatur',
+                  'Doraville',
+                  'Dunwoody',
+                  'East Point',
+                  'Ellenwood',
+                  'Fairburn',
+                  'Forest Park',
+                  'Hapeville',
+                  'Jonesboro',
+                  'Lake City',
+                  'Lovejoy',
+                  'Lithonia',
+                  'Morrow',
+                  'Palmetto',
+                  'Riverdale',
+                  'Roswell',
+                  'Sandy Springs',
+                  'Stone Mountain',
+                  'Union City']
+
   def full_street_address
     "#{street_address}, #{city}, GA"
   end
